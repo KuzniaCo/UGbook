0cdeba57a350c23886c1d19b3cce8f1b83020097
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-04-09 12:45:26 +0200

    Update comment.schema.ts

--- ugbook-back/src/schemas/comment.schema.ts
+++ ugbook-back/src/schemas/comment.schema.ts
@@ -1,5 +1,5 @@
-import { Prop, Schema, SchemaFactory, unique } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, ObjectId } from 'mongoose';
 
 export type CommentDocument = Comment & Document;
 
@@ -7,8 +7,7 @@ export type CommentDocument = Comment & Document;
 export class Comment {
     
     @Prop({ required: true })
-    @unique()
-    authorId: string;
+    authorId: ObjectId
 
     @Prop({ required: true })
     content: string;
